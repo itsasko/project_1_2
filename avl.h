21bd771a44eef2d65f4d3e6238c1eaed775a3288
Oksana <oksa.osidach@gmail.com>
2023-12-19 23:17:08 +0100

    AVL, highlighting 4 cases: right-right, right-left, left-right, left-left

--- avl.h
+++ avl.h
@@ -1,12 +1,10 @@
 #ifndef PROJECT_1_2_AVL_H
 #define PROJECT_1_2_AVL_H
-
 struct Node_avl {
     int value, height = 0;
     Node_avl *left = nullptr;
     Node_avl *right = nullptr;
 };
-
 void delete_tree(Node_avl *root) {
     if (root != nullptr) {
         delete_tree(root->left);
@@ -23,29 +21,27 @@ int avl_balanced(Node_avl* tmp){
     int difference = left_height - right_height;
     return difference;
 }
-
 int avl_height_calc(Node_avl* tmp){
     if (tmp == nullptr) return -1;
     return std::max(avl_height_calc(tmp->left), avl_height_calc(tmp->right)) + 1;
 }
-
 Node_avl* & left_rotation(Node_avl* &tmp){ // subtree > 1 => left-right; subtree < -1 => left-left
-        Node_avl* x = tmp;
-        Node_avl* y = tmp->right;            // left rotation
-        Node_avl* y_left = y->left;
-        tmp = y;
-        y->left = x;
-        x->right = y_left;
-        return y;
+    Node_avl* x = tmp;
+    Node_avl* y = tmp->right;            // left rotation
+    Node_avl* y_left = y->left;
+    tmp = y;
+    y->left = x;
+    x->right = y_left;
+    return y;
 }
 Node_avl* & right_rotation(Node_avl* &tmp){ // subtree > 1 => right-right; subtree < -1 => right-left
-        Node_avl* x = tmp;
-        Node_avl* y = tmp->left;            // right rotation
-        Node_avl* y_right = y->right;
-        tmp = y;
-        y->right = x;
-        x->left = y_right;
-        return y;
+    Node_avl* x = tmp;
+    Node_avl* y = tmp->left;            // right rotation
+    Node_avl* y_right = y->right;
+    tmp = y;
+    y->right = x;
+    x->left = y_right;
+    return y;
 }
 
 void avl_insert(Node_avl* &root, int element){
@@ -54,12 +50,19 @@ void avl_insert(Node_avl* &root, int element){
             root->left = new Node_avl;
             root->left->value = element;
             root->height = avl_height_calc(root);
-            int balance = avl_balanced(root->left);
-            if(balance > 1){
-                root->right = left_rotation(root->right);
-            } // left rotation
-            if(balance < -1) {
-                root->left = left_rotation(root->left);
+            int balance = avl_balanced(root);
+            int balance_2;
+            if(balance > 1){ // left-
+                balance_2 = avl_balanced(root->left);
+                if(balance_2 > 1) root->left = right_rotation(root->left); // left-left case
+                if(balance_2 < -1) root->left = left_rotation(root->left); // left-right case
+                root = right_rotation(root);
+            }
+            if(balance < -1){ // right-
+                balance_2 = avl_balanced(root->right);
+                if(balance_2 > 1) root->right = right_rotation(root->left); // right-left case
+                if(balance_2 < -1) root->right = left_rotation(root->left); // right-right case
+                root = left_rotation(root);
             }
         }
         else{
@@ -71,24 +74,19 @@ void avl_insert(Node_avl* &root, int element){
             root->right = new Node_avl;
             root->right->value = element;
             root->height = avl_height_calc(root);
-            int balance = avl_balanced(root->right);
+            int balance = avl_balanced(root);
             if(balance > 1){
-                root->right = right_rotation(root->right);
-            } // right rotation
-            if(balance < -1){
-                root->left = right_rotation(root->left);
+                root->right = left_rotation(root->right);
+            } // left rotation
+            if(balance < -1) {
+                root->left = left_rotation(root->left);
             }
-        }
+        } // right rotation
         else{
             avl_insert(root->right, element);
         }
-        root->height = avl_height_calc(root);
-        int whole_tree_balance = avl_balanced(root);
-        if(whole_tree_balance > 1) root = right_rotation(root);
-        if(whole_tree_balance < -1) root = left_rotation(root);
     }
 }
-
 Node_avl* avl_build(std::vector<int>& numbers){
     Node_avl *root = new Node_avl;
     root->value = numbers[0];
@@ -98,7 +96,6 @@ Node_avl* avl_build(std::vector<int>& numbers){
     }
     return root;
 }
-
 void avl_output(Node_avl* &tmp, std::vector<int>& sorted){
     if(tmp != nullptr){
         avl_output(tmp->left, sorted);
@@ -107,7 +104,6 @@ void avl_output(Node_avl* &tmp, std::vector<int>& sorted){
     }
     return;
 }
-
 void avl(std::vector<int>& numbers){
     Node_avl* tmp = avl_build(numbers);
     Node_avl* tmp_ = tmp;
@@ -115,5 +111,4 @@ void avl(std::vector<int>& numbers){
     avl_output(tmp, numbers);
     delete_tree(tmp_);
 }
-
 #endif //PROJECT_1_2_AVL_H
